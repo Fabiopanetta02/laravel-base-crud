a53e1a5129debfb695dc63638180b4529a2f5334
Fabiopanetta02 <panetta.fabio20@gmail.com>
2022-09-21 14:26:48 +0200

    Added comic with db

---
+++ resources/views/comics/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.main')
+
+@section('title', 'Comics')
+    
+@section('content')
+<div class="d-flex flex-wrap">
+    @forelse ($comics as $comic)
+    <div class="card mx-3 my-5" style="width: 18rem;">
+        <div class="card-body">
+            @if($comic->thumb) 
+                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}" class="img-fluid">
+            @endif
+            <h5 class="card-title">{{ $comic->title }}</h5>
+            <h6 class="card-subtitle mb-2 text-muted">{{ $comic->series }}</h6>
+            <p class="card-text">{{ $comic->description }}</p>
+            <p class="card-text">{{ $comic->sale_date}}</p>
+            <p class="card-text">{{ $comic->price}}</p>
+            <p class="card-text">{{ $comic->type}}</p>
+        </div>
+    </div>
+    @empty
+    @endforelse
+</div>
+
+@endsection
